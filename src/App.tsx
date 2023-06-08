a54a8ecb171427ba8ed5adb4a65321fc4c15af58
ahiipsa <ahiipsa@gmail.com>
2023-06-08 23:50:44 +0400

    gnosis safe

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { StakingContract, StakingAPI, NETWORK_TYPE } from "harmony-staking-sdk";
-import { getChainsConfig, getBalances, proposeTransaction, getTransactionQueue, setBaseUrl, type ChainListResponse } from '@safe-global/safe-gateway-typescript-sdk'
+import { StakingAPI, NETWORK_TYPE } from "harmony-staking-sdk";
+import { getChainsConfig, getBalances, setBaseUrl } from '@safe-global/safe-gateway-typescript-sdk'
 import Web3 from "web3";
+import Safe, { Web3Adapter } from '@safe-global/protocol-kit'
 import './App.css';
+import {MetaTransactionData, SafeTransactionDataPartial} from "@safe-global/safe-core-sdk-types";
+import {StakingContract} from "./staking/contract";
+import {MultisigTransactionRequest} from "@safe-global/safe-gateway-typescript-sdk/dist/types/transactions";
+import SafeApiKit from "@safe-global/api-kit";
+
+
+const SAFE_GATEWAY_URL = 'https://gateway.multisig.harmony.one'
+const SAFE_TRANSACTION_URL = 'https://transaction.multisig.harmony.one'
 
 setBaseUrl('https://gateway.multisig.harmony.one/')
 
@@ -23,6 +32,19 @@ const stakingContract = new StakingContract({
   provider: web3.currentProvider as any,
 });
 
+const ethAdapter = new Web3Adapter({
+  web3,
+  signerAddress: '0xCBB2CDFa7551650B767f12527D54B221176E26a8'
+})
+
+const safeAddress = '0x3Fb8cFB3EAeE90E26B3eC8136eF6E90696CFD1DD'
+const validatorAddress = '0xe05941d0919d058cfbb173655178201721002dbb'
+
+console.log('### SafeApiKit', SafeApiKit);
+const safeService = new SafeApiKit({ txServiceUrl: SAFE_TRANSACTION_URL, ethAdapter })
+
+// @ts-ignore
+window.ss = safeService;
 
 const lib = {
   getDelegations: async (address: string, validatorAddress: string) => {
@@ -42,7 +64,6 @@ const lib = {
   },
   collectRewards: async() => {
       return stakingContract.collectRewards((txHash) => {
-
         console.log('### txHash', txHash);
       })
   },
@@ -69,9 +90,81 @@ const lib = {
   safeGetBalances: async (safeAddress: string) => {
     return await getBalances('1666600000', safeAddress);
   },
-  proposeTransaction: () => {
-    proposeTransaction()
+  safeDelegate: async (senderAddress: string, validatorAddress: string, safeAddress: string, amount: string) => {
+    const safeSdk = await Safe.create({ ethAdapter, safeAddress })
+    const data = stakingContract.delegateEncodeABI(safeAddress, validatorAddress, amount);
+
+    const checksummedAddress = web3.utils.toChecksumAddress(stakingContract.getContractAddress());
+
+    const safeTransactionData: SafeTransactionDataPartial = {
+      to:checksummedAddress,
+      data,
+      value: '0',
+    }
+
+    const safeTransaction = await safeSdk.createTransaction({ safeTransactionData })
+
+    const safeTxHash = await safeSdk.getTransactionHash(safeTransaction)
+    const senderSignature = await safeSdk.signTransactionHash(safeTxHash)
+    await safeService.proposeTransaction({
+      safeAddress,
+      safeTransactionData: safeTransaction.data,
+      safeTxHash,
+      senderAddress,
+      senderSignature: senderSignature.data,
+    })
+  },
+
+  safeUndelegate: async (senderAddress: string, validatorAddress: string, safeAddress: string, amount: string) => {
+    const safeSdk = await Safe.create({ ethAdapter, safeAddress })
+    const data = stakingContract.unDelegateEncodeABI(safeAddress, validatorAddress, amount);
+
+    const checksummedAddress = web3.utils.toChecksumAddress(stakingContract.getContractAddress());
+
+    const safeTransactionData: SafeTransactionDataPartial = {
+      to:checksummedAddress,
+      data,
+      value: '0',
+    }
+
+    const safeTransaction = await safeSdk.createTransaction({ safeTransactionData })
+
+    const safeTxHash = await safeSdk.getTransactionHash(safeTransaction)
+    const senderSignature = await safeSdk.signTransactionHash(safeTxHash)
+    await safeService.proposeTransaction({
+      safeAddress,
+      safeTransactionData: safeTransaction.data,
+      safeTxHash,
+      senderAddress,
+      senderSignature: senderSignature.data,
+    })
+  },
+
+  safeCollectRewards: async (senderAddress: string, safeAddress: string) => {
+    const safeSdk = await Safe.create({ ethAdapter, safeAddress })
+    const data = stakingContract.collectRewardsEncodeABI(safeAddress);
+
+    const checksummedAddress = web3.utils.toChecksumAddress(stakingContract.getContractAddress());
+
+    const safeTransactionData: SafeTransactionDataPartial = {
+      to:checksummedAddress,
+      data,
+      value: '0',
+    }
+
+    const safeTransaction = await safeSdk.createTransaction({ safeTransactionData })
+
+    const safeTxHash = await safeSdk.getTransactionHash(safeTransaction)
+    const senderSignature = await safeSdk.signTransactionHash(safeTxHash)
+    await safeService.proposeTransaction({
+      safeAddress,
+      safeTransactionData: safeTransaction.data,
+      safeTxHash,
+      senderAddress,
+      senderSignature: senderSignature.data,
+    })
   }
+
 }
 
 // @ts-ignore
